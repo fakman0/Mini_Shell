b15c083029c708725c5ab43ced3a243f89351e47
FurkanATR <FurkanATR23@gmail.com>
2023-07-06 01:12:43 +0300

    Expander Up

--- expander/expander.c
+++ expander/expander.c
@@ -1,45 +1,19 @@
 #include "../minishell.h"
 
-static void	expand_tilda(t_list **splitted_str, char **env, int i, int j)
+static void	expand_tilda(t_list **splitted_str, int i, int k)
 {
 	char	*new_str;
-	char	*var;
-	t_env	*envs;
+	char	c;
 	t_list	*iter;
-	int		k;
 
-	envs = env_variables(env, 0);
 	iter = *splitted_str;
-	var = envs->tilda;
 	while (iter)
 	{
 		k = 0;
 		i = 0;
 		new_str = malloc(sizeof(char) * 10000);
 		while (iter->command[i])
-		{
-			j = 0;
-			if (iter->command[i] == '\'')
-			{
-				new_str[k++] = iter->command[i++];
-				while (iter->command[i] != '\0' && iter->command[i] != '\'')
-					new_str[k++] = iter->command[i++];
-			}
-			if (iter->command[i] == '\"')
-			{
-				new_str[k++] = iter->command[i++];
-				while (iter->command[i] != '\0' && iter->command[i] != '\"')
-					new_str[k++] = iter->command[i++];
-			}
-			if (iter->command[i] == '~')
-			{
-				while (var[j])
-					new_str[k++] = var[j++];
-				i++;
-			}
-			else
-				new_str[k++] = iter->command[i++];
-		}
+			tilda_extra(iter, &i, new_str, &k);
 		new_str[k] = '\0';
 		new_str = ft_str_realloc(new_str);
 		new_command(iter, new_str);
@@ -48,35 +22,7 @@ static void	expand_tilda(t_list **splitted_str, char **env, int i, int j)
 	}
 }
 
-void	expand_dollar(t_list *it, int *i, char *new_str, int *j)
-{
-	char	*env_key;
-	char	*env_var;
-
-	*i = *i + 1;
-	data.counter = 0;
-	env_key = malloc(sizeof(char ) * 100);
-	while (it->command[*i] != ' ' && it->command[*i] != '\''
-		&& it->command[*i] != '\"' && it->command[*i] != '\0'
-		&& it->command[*i] != '$')
-	{
-			env_key[data.counter++] = it->command[*i];
-			*i = *i + 1;
-	}
-	env_key[data.counter] = '\0';
-	env_var = find_values(data.env, env_key);
-	if (env_var != NULL)
-	{
-		data.counter = 0;
-		while (env_var[data.counter])
-		{
-			new_str[*j] = env_var[data.counter++];
-			*j = *j + 1;
-		}
-	}
-}
-
-void	expand_var(t_list **splitted_str, int i, int j)
+static void	expand_var(t_list **splitted_str, int i, int j)
 {
 	t_list	*iter;
 	char	*new_str;
@@ -90,31 +36,11 @@ void	expand_var(t_list **splitted_str, int i, int j)
 		while (iter->command[i])
 		{
 			if (iter->command[i] == '\"')
-			{
-				i++;
-				while (iter->command[i] != '\"' && iter->command[i])
-				{
-					if (iter->command[i] == '$')
-						expand_dollar(iter, &i, new_str, &j);
-					else
-						new_str[j++] = iter->command[i++];
-				}
-				i++;
-			}
+				expand_quote(iter, &i, new_str, &j);
 			else if (iter->command[i] == '\'')
-			{
-				i++;
-				while (iter->command[i] != '\'' && iter->command[i])
-					new_str[j++] = iter->command[i++];
-				i++;
-			}
+				expand_single_quote(iter, &i, new_str, &j);
 			else
-			{
-				if (iter->command[i] == '$')
-					expand_dollar(iter, &i, new_str, &j);
-				else
-					new_str[j++] = iter->command[i++];
-			}
+				expand_normal(iter, &i, new_str, &j);
 		}
 		new_str[j] = '\0';
 		new_command(iter, new_str);
@@ -127,6 +53,6 @@ void	expander(t_list **splitted_str, char **env)
 	t_env	*envs;
 
 	envs = env_variables(env, 0);
-	expand_tilda(splitted_str, env, 0, 0);
+	expand_tilda(splitted_str, 0, 0);
 	expand_var(splitted_str, 0, 0);
 }
